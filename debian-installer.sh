dabb5294935299f32e37e7db2db95ba2b05ab3ec
Damian <kontakt@dszymczuk.pl>
2018-09-27 11:38:45 +0200

    add user for run docker

--- debian-installer.sh
+++ debian-installer.sh
@@ -115,6 +115,10 @@ apt-get update
 
 apt-get install -y docker-ce
 
+groupadd docker
+
+usermod -aG docker damian
+
 ## Docker compose install
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
