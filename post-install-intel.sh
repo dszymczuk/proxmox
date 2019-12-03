8bb8a87c117af76401bf1f855df63c5f2ca998d0
Damian <kontakt@dszymczuk.pl>
2019-12-03 16:39:21 +0100

    add post install proxmox files instead of env

--- post-install-intel.sh
+++ post-install-intel.sh
@@ -174,10 +174,6 @@ kernel.keys.root_maxkeys=1000000
 kernel.keys.maxkeys=1000000
 EOF
 
-## install pigz
-## https://blog.kowalsio.com/2018/04/23/przyspieszanie-backupu-maszyn-wirtualnych-w-proxmox-ve-kompresja-przy-pomocy-pigz/
-
-apt-get -y install pigz
 
 ## Increafe vzdump size
 cat <<'EOF' >> /etc/vzdump.conf
@@ -187,7 +183,6 @@ cat <<'EOF' >> /etc/vzdump.conf
 # https://stackoverflow.com/questions/25449019/snapshot-backup-of-25gb-container-openvz-proxmox
 #
 size: 4096
-pigz: 1
 
 EOF
 
